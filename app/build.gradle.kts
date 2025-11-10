ed03b4024e672b19a81900afac0f0cba7575c754
SAUGAT <saugatpokharelli@gmail.com>
2025-11-10 09:58:12 +0000

    Added Google Maps dependencies, permissions, and API key configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation("com.google.android.gms:play-services-maps:19.0.0")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+
+
 }
